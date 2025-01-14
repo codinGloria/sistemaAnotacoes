323bd7172f397c53ae7f19055b8c2e6f48ab7661
codinGloria <codingloria@gmail.com>
2025-01-14 10:09:10 -0300

    ✨ feat: Sistema completo... ou quase?

--- server.js
+++ server.js
@@ -1,3 +1,36 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+let anotacoes = [
+    { id: 0, texto: "Sou uma anotação"}
+];
+
+let ultimoId = 1;
+
+app.get('/anotacoes', (req, res) => {
+    res.json(anotacoes);
+});
+
+app.post('/anotacoes', (req, res) => {
+    ultimoId++;
+    anotacoes.push({ id: ultimoId, texto: req.body.texto });
+    res.json({info: "Anotação criada com sucesso!"});
+});
+
+app.delete('/anotacoes/:id', (req, res) => {
+    anotacoes = anotacoes.filter(({id}) => id != +req.params.id);
+    res.json({
+        info: `Anotação de id: ${req.params.id} deletada com sucesso!`
+    });
+});
+
+const port = 5500;
+const host = "127.0.0.1";
+
+app.listen(port, host, () => {
+    console.log(`Servidor inicializado em http://${host}:${port}`);
+});
